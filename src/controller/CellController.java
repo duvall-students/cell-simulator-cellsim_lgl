e45df20b289419682d1269488f83d714ed765d20
lukefreudenthal <lukefreudenthal@localhost>
2022-11-20 16:33:34 -0500

    Beginning to work on incorporating CellBoard and Cell Functionality

--- src/controller/CellController.java
+++ src/controller/CellController.java
@@ -1,46 +1,61 @@
 package controller;
 
+import View.UI;
 import model.Cell;
 import model.CellBoard;
 
 public class CellController {
 	
 	// Model
-	private CellBoard cellBoard;
+	private CellBoard board;
 	private Cell cell;
 	
 	private int rows;
 	private int cols;
 	
 	// View
-	// private UI cellView;
-
-	public CellController() {
-		// TODO Auto-generated constructor stub
-	}
+	private UI display;
 	
-	public void startSimulation() {
-		
+	// Controller
+	private Cell[][] savedBoard;
+	private boolean runStatus;
+
+	public CellController(int rows, int cols, UI display) {
+		board = new CellBoard(rows, cols);
+		this.display = display;
 	}
 	
 	public void newBoard() {
-		
+		board.randomizeBoard();
+//		display.redraw();
+	}
+	
+	public void startSimulation() {
+		board.update();
 	}
 	
 	public void playSimulation() {
-		
+		runStatus = true;
 	}
 	
 	public void pauseSimulation() {
+		runStatus = false;
+	}
+	
+	public void step() {
+		
+	}
+	
+	public void doOneStep() {
 		
 	}
 	
 	public void saveSimulation() {
-		
+		savedBoard = board.copyBoard();
 	}
 	
 	public void loadSimulation() {
-		
+		board.setBoard(savedBoard);
 	}
 
 }
